dce7937f06f898afb74c6defd9546519d9c75f6f
Giovanni Fernandes Vicentin <giovannifvicentin@gmail.com>
2023-09-05 17:41:05 -0300

    Mapping through data with hooks in SearchParams

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import SearchParams from "../SearchParams";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// @ts-nocheck
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react()],
-    root: "src",
+  plugins: [react()],
+  root: "src",
 });
